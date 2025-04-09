eb1ebba0987ac77fb46d075a385388146f3243e3
Adithyahari vpn <68626434+Adithyaharivpn@users.noreply.github.com>
2025-04-09 13:45:30 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ var app= express();
 app.use(cors());
 
 app.use(express.json())
-var port=3000;
+const port = process.env.PORT || 3000;
 
 app.use('/api',userRoute)
 app.use('/product',productRoute)
